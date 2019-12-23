316e42204529d96fa52b906a4f16a57e72767572
Sheng-Jie106 <55180712+Sheng-Jie106@users.noreply.github.com>
2019-12-24 01:00:31 +0800

    4

--- assert/js/sideshow.js
+++ assert/js/sideshow.js
@@ -18,11 +18,13 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-}
+  setTimeout(showSlides, 3000);}
